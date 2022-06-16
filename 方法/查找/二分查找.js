f13978417f9ddae5ae84ccab7609bb9c8e41cd9c
wskkku <wshhh250@gmail.com>
2022-06-16 15:17:24 +0800

    Create 二分查找.js

---
+++ 方法/查找/二分查找.js
@@ -0,0 +1,18 @@
+var search = function (nums, target) {
+  var left = 0;
+  var right = nums.length - 1;
+  while (left <= right) {
+    var mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+// https://leetcode.cn/problems/binary-search/solution/er-fen-cha-zhao-xiang-jie-by-labuladong/
